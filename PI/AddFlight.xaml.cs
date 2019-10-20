7558f0489aeb83a51966b112dd0d8e392b332e87
name <vadimgromov1403@gmail.com>
2019-10-21 02:12:41 +0300

    Fix code

--- PI/AddFlight.xaml.cs
+++ PI/AddFlight.xaml.cs
@@ -85,6 +85,7 @@ namespace PI
             {
                 try
                 {
+                    MessageBox.Show(DepartDate.Text);
                     string connectionString = ConfigurationManager.ConnectionStrings["MainConnection"].ConnectionString;
                     string query = $"INSERT INTO Flight (DepartCity,ArriveCity,DepartDate,ArriveDate,DepartTime,ArriveTime,AirplaneID,Airline) " +
                         $"VALUES ('{DepartCityComboBox.Text}','{ArrivalCityComboBox.Text}','{DepartDate.Text}','{ArrivalDate.Text}','{DepartTimePicker.Text}','{ArrivalTimePicker.Text}','{AiplaneIdComboBox.Text}','{AirlineBox.Text}')";
